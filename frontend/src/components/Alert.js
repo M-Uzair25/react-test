e9413195ffba8429b8ee57a39770e2176462d490
Muhammad Uzair <m.uzair25@outlook.com>
2022-09-06 15:16:37 +0500

    Frontend-completed

--- frontend/src/components/Alert.js
+++ frontend/src/components/Alert.js
@@ -3,7 +3,7 @@ import React from 'react'
 
 function Alert(props) {
       return (
-        <div style={{height:"35px"}} > 
+        <div style={{height:"35px", position:'relative', top:'50px'}} > 
         {props.alert && 
          <div className={`alert alert-${props.alert.type} `} role="alert">
             <div> {props.alert.msg}</div>
